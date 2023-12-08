ddff3d518ae00979c2dbabb713a85e90cc769378
Ba-koD <rudghrnt12@naver.com>
2023-12-08 12:51:46 +0900

    Feat: Add Laserpointer Detect

--- LaserDetect/laser.c
+++ LaserDetect/laser.c
@@ -4,11 +4,13 @@
 #include <opencv2/imgproc/imgproc_c.h>
 
 int main() {
-    CvCapture* capture = cvCaptureFromCAM(0);
+    CvCapture* capture = cvCaptureFromCAM(0); // '0'은 첫 번째 카메라 장치를 의미합니다.
     if (!capture) {
         fprintf(stderr, "카메라를 열 수 없습니다.\n");
         return -1;
     }
+
+    cvSetCaptureProperty(capture, CV_CAP_PROP_FPS, 60);
 
     cvNamedWindow("타겟 인식", CV_WINDOW_AUTOSIZE);
     cvNamedWindow("처리된 영상", CV_WINDOW_AUTOSIZE);
@@ -19,6 +21,16 @@ int main() {
     IplImage* edge;
     CvMemStorage* storage = cvCreateMemStorage(0);
 
+    // 빨간색 필터링을 위한 HSV 범위
+    CvScalar lower_red = cvScalar(0, 120, 70, 0);
+    CvScalar upper_red = cvScalar(10, 255, 255, 0);
+    CvScalar lower_red2 = cvScalar(160, 120, 70, 0);
+    CvScalar upper_red2 = cvScalar(180, 255, 255, 0);
+
+    IplImage* hsv;
+    IplImage* redMask;
+    IplImage* redMask2;
+
     while (1) {
         frame = cvQueryFrame(capture);
         if (!frame) break;
@@ -27,9 +39,23 @@ int main() {
         blurred = cvCreateImage(cvGetSize(frame), IPL_DEPTH_8U, 1);
         edge = cvCreateImage(cvGetSize(frame), IPL_DEPTH_8U, 1);
 
+        hsv = cvCreateImage(cvGetSize(frame), IPL_DEPTH_8U, 3);
+        redMask = cvCreateImage(cvGetSize(frame), IPL_DEPTH_8U, 1);
+        redMask2 = cvCreateImage(cvGetSize(frame), IPL_DEPTH_8U, 1);
+
         cvCvtColor(frame, gray, CV_BGR2GRAY);
         cvSmooth(gray, blurred, CV_GAUSSIAN, 9, 9, 0, 0);
         cvCanny(blurred, edge, 5, 30, 3);
+
+        // BGR을 HSV로 변환
+        cvCvtColor(frame, hsv, CV_BGR2HSV);
+
+        // 빨간색 범위 필터링
+        cvInRangeS(hsv, lower_red, upper_red, redMask);
+        cvInRangeS(hsv, lower_red2, upper_red2, redMask2);
+
+        // 두 마스크 결합
+        cvOr(redMask, redMask2, redMask, NULL);
 
         CvSeq* contours;
         cvFindContours(edge, storage, &contours, sizeof(CvContour), CV_RETR_LIST, CV_CHAIN_APPROX_SIMPLE, cvPoint(0, 0));
@@ -48,6 +74,9 @@ int main() {
                     if (circularity > 0.75) {
                         CvBox2D ellipse = cvFitEllipse2(contours);
                         cvEllipseBox(processed, ellipse, CV_RGB(255, 0, 0), 2, 8, 0);
+
+                        // 타겟 영역 안에 빨간색이 있는지 확인
+                        // 이 부분은 타겟의 위치와 크기를 기반으로 추가 로직을 작성해야 합니다.
                     }
                 }
             }
@@ -65,6 +94,9 @@ int main() {
         cvReleaseImage(&gray);
         cvReleaseImage(&blurred);
         cvReleaseImage(&edge);
+        cvReleaseImage(&hsv);
+        cvReleaseImage(&redMask);
+        cvReleaseImage(&redMask2);
     }
 
     cvReleaseCapture(&capture);
