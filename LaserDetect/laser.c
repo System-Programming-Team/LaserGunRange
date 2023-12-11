ed5e40eaeb06b7ba6c90fd93b14e840c73886492
Ba-koD <rudghrnt12@naver.com>
2023-12-11 11:24:19 +0900

    Refactor: Refactoring Code

--- LaserDetect/laser.c
+++ LaserDetect/laser.c
@@ -1,22 +1,22 @@
-#include <stdio.h>
-#include <math.h>
-#include <opencv2/highgui/highgui_c.h>
-#include <opencv2/imgproc/imgproc_c.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include <pthread.h>
-#include <arpa/inet.h>
-#include <sys/socket.h>
-#include <stdlib.h>
-#include <stdint.h>
-#include <stdbool.h>
-#include <time.h>
+#include<stdio.h>
+#include<math.h>
+#include<opencv2/highgui/highgui_c.h>
+#include<opencv2/imgproc/imgproc_c.h>
+#include<unistd.h>
+#include<stdlib.h>
+#include<pthread.h>
+#include<arpa/inet.h>
+#include<sys/socket.h>
+#include<stdlib.h>
+#include<stdint.h>
+#include<stdbool.h>
+#include<time.h>
 
 bool waiting = true;
 uint8_t led;
 
 #define CLIENT 0
-#define OPEN 1
+#define OPEN 0
 
 void *communicate_data(void *sock);
 
@@ -27,30 +27,27 @@ typedef struct{
 }Coordinate;
 
 void check_and_add(Coordinate coords[], int *size, Coordinate new_coords){
-    for(int i = 0; i < *size; i++) {
-        if(abs(coords[i].index - new_coords.index) <= 3) {
-            return; // 새로운 값과 기존 값의 차이가 3 이하라면 함수 종료
-        }
+    for(int i=0;i<*size;i++){
+        if(abs(coords[i].index - new_coords.index) <= 3) return; // 새로운 값과 기존 값의 차이가 3 이하라면 함수 종료
     }
-
     // 새로운 값과 기존 값의 차이가 모두 3 초과라면 배열에 추가
-    coords[*size] = new_coords;
+    coords[*size]=new_coords;
     (*size)++;
 }
 // 사용자 정의 비교 함수
-int compare(const void *a, const void *b) {
-    Coordinate *coordA = (Coordinate *)a;
-    Coordinate *coordB = (Coordinate *)b;
+int compare(const void *a, const void *b){
+    Coordinate *coordA=(Coordinate *)a;
+    Coordinate *coordB=(Coordinate *)b;
 
-    if (coordA->indeh < coordB->indeh) return 1;
-    else if (coordA->indeh > coordB->indeh) return -1;
+    if(coordA->indeh < coordB->indeh) return 1;
+    else if(coordA->indeh > coordB->indeh) return -1;
     else return 0;
 }
 
 int main(int argc, char *argv[]){
 #if CLIENT
-    if (argc != 3) {
-        fprintf(stderr, "Usage: %s <IP> <port>\n", argv[0]);
+    if(argc!=3){
+        fprintf(stderr, "Usage : %s <IP> <port>\n", argv[0]);
         exit(1);
     }
     // 파일 디스크립터, 포트, 서버 주소, send, receive 스레드 저장할 변수 선언
@@ -62,7 +59,7 @@ int main(int argc, char *argv[]){
     server_addr.sin_family=AF_INET;
     server_addr.sin_port=htons(port);
 
-        // argument에서 받아온 주소 저장
+    // argument에서 받아온 주소 저장
     inet_pton(AF_INET, argv[1], &server_addr.sin_addr);
 
     // 제공된 정보를 기반으로 서버에 연결요청.
@@ -73,57 +70,57 @@ int main(int argc, char *argv[]){
 #endif
     time_t start,end;
     Coordinate coords[10];
-    CvCapture* capture = cvCaptureFromCAM(0);
-    if (!capture) {
+    CvCapture* capture=cvCaptureFromCAM(0);
+    if(!capture){
         fprintf(stderr, "카메라를 열 수 없습니다.\n");
         return -1;
     }
 #if OPEN
-    cvNamedWindow("타겟 인식", CV_WINDOW_AUTOSIZE);
-    cvNamedWindow("처리된 영상", CV_WINDOW_AUTOSIZE);
+    printf("Open window1\n");
+    cvNamedWindow("Raw Image", CV_WINDOW_AUTOSIZE);
+    printf("Open window2\n");
+    cvNamedWindow("Processed Image", CV_WINDOW_AUTOSIZE);
+    printf("Opening Done\n");
 #endif
     IplImage* frame;
     IplImage* gray;
     IplImage* blurred;
     IplImage* edge;
 
-    CvMemStorage* storage = cvCreateMemStorage(0);
+    CvMemStorage* storage=cvCreateMemStorage(0);
     
-    int score = 0;
+    int score=0;
     
-    while (1) {
-        frame = cvQueryFrame(capture);
-        if (!frame) break;
+    while(1){
+        frame=cvQueryFrame(capture);
+        if(!frame) break;
         int point=0;
-        uint send, recv=3;
+        uint send, recv;
     
-        gray = cvCreateImage(cvGetSize(frame), IPL_DEPTH_8U, 1);
-        blurred = cvCreateImage(cvGetSize(frame), IPL_DEPTH_8U, 1);
-        edge = cvCreateImage(cvGetSize(frame), IPL_DEPTH_8U, 1);
+        gray=cvCreateImage(cvGetSize(frame), IPL_DEPTH_8U, 1);
+        blurred=cvCreateImage(cvGetSize(frame), IPL_DEPTH_8U, 1);
+        edge=cvCreateImage(cvGetSize(frame), IPL_DEPTH_8U, 1);
     
         cvCvtColor(frame, gray, CV_BGR2GRAY);
         cvSmooth(gray, blurred, CV_GAUSSIAN, 9, 9, 0, 0);
         cvCanny(blurred, edge, 5, 30, 3);
     
-        CvSeq* contours1;
-        CvSeq* contours2;
-        cvFindContours(edge, storage, &contours1, sizeof(CvContour), CV_RETR_LIST, CV_CHAIN_APPROX_SIMPLE, cvPoint(0, 0));
-        cvFindContours(edge, storage, &contours2, sizeof(CvContour), CV_RETR_LIST, CV_CHAIN_APPROX_SIMPLE, cvPoint(0, 0));
+        CvSeq* contours;
+        cvFindContours(edge, storage, &contours, sizeof(CvContour), CV_RETR_LIST, CV_CHAIN_APPROX_SIMPLE, cvPoint(0, 0));
     
-        // 처리된 이미지 준비
-        IplImage* processed = cvCreateImage(cvGetSize(frame), IPL_DEPTH_8U, 3);
-        cvSet(processed, cvScalar(255, 255, 255, 0), NULL); // 하얀색으로 초기화
+        IplImage* processed=cvCreateImage(cvGetSize(frame), IPL_DEPTH_8U, 3);
+        cvSet(processed, cvScalar(255, 255, 255, 0), NULL);
 
-        while(contours1){
-            if(contours1->total>=5){
-                double area = cvContourArea(contours1, CV_WHOLE_SEQ, 0);
-                double perimeter = cvArcLength(contours1, CV_WHOLE_SEQ, 1);
-                double circularity = 4 * M_PI * area / (perimeter * perimeter);
+        while(contours){
+            if(contours->total>=5){
+                double area=cvContourArea(contours, CV_WHOLE_SEQ, 0);
+                double perimeter=cvArcLength(contours, CV_WHOLE_SEQ, 1);
+                double circularity=4*M_PI*area/(perimeter * perimeter);
 
                 CvBox2D ellipseCircle;
 
-                if(area>200 && circularity > 0.3){
-                    ellipseCircle = cvFitEllipse2(contours1);
+                if(area>200 && circularity>0.3){
+                    ellipseCircle = cvFitEllipse2(contours);
                     cvEllipseBox(processed, ellipseCircle, CV_RGB(0, 255, 0), 2, 8, 0);  // 초록색 원 그리기
                     Coordinate new_coords;
                     new_coords.index=ellipseCircle.center.x;
@@ -132,24 +129,25 @@ int main(int argc, char *argv[]){
                     check_and_add(coords, &point, new_coords);
                 }
             }
-            contours1=contours1->h_next;
+            contours=contours->h_next;
         }
-        qsort(coords, point, sizeof(Coordinate), compare); // qsort 함수로 정렬
+        qsort(coords, point, sizeof(Coordinate), compare);
+        cvFindContours(edge, storage, &contours, sizeof(CvContour), CV_RETR_LIST, CV_CHAIN_APPROX_SIMPLE, cvPoint(0, 0));
 
-        while (contours2) {
-            if (contours2->total >= 5) {
-                double area = cvContourArea(contours2, CV_WHOLE_SEQ, 0);
-                double perimeter = cvArcLength(contours2, CV_WHOLE_SEQ, 1);
-                double circularity = 4 * M_PI * area / (perimeter * perimeter);
-                
-                CvBox2D ellipseLaser;  // 레이저 포인트의 원을 나타내는 변수
-                
-                if (area <= 200 && circularity > 0.3) { // 레이저 포인트를 원으로 인식 (크기가 20 이하)
-                    ellipseLaser = cvFitEllipse2(contours2);
+        while(contours){
+            if(contours->total >= 5){
+                double area=cvContourArea(contours, CV_WHOLE_SEQ, 0);
+                double perimeter=cvArcLength(contours, CV_WHOLE_SEQ, 1);
+                double circularity=4*M_PI*area / (perimeter * perimeter);
+
+                CvBox2D ellipseLaser;
+
+                if(area<=200 && circularity>0.3){
+                    ellipseLaser=cvFitEllipse2(contours);
                     cvEllipseBox(processed, ellipseLaser, CV_RGB(255, 0, 0), 2, 8, 0);  // 레이저 포인트를 빨간색 원으로 표시
                     for(int i=0;i<point;i++){
-                        double dx=ellipseLaser.center.x - coords[i].index;
-                        double dy=ellipseLaser.center.y - coords[i].indey;
+                        double dx=ellipseLaser.center.x-coords[i].index;
+                        double dy=ellipseLaser.center.y-coords[i].indey;
                         double dist=sqrt(dx*dx + dy*dy);
                         end=time(NULL);
                         if(dist<coords[i].indeh/2 && ellipseLaser.center.x!=0 && ellipseLaser.center.y!=0 && difftime(end,start)>0.5){
@@ -160,7 +158,7 @@ int main(int argc, char *argv[]){
                                 waiting=false;
                             }
                             #endif
-                            ellipseLaser.center.x=0,ellipseLaser.center.y=0;
+                            ellipseLaser.center.x=0, ellipseLaser.center.y=0;
                             start=time(NULL);
                             break;
                         }
@@ -173,31 +171,27 @@ int main(int argc, char *argv[]){
                     #endif
                 }
             }
-            contours2 = contours2->h_next;
+            contours = contours->h_next;
         }
 
 #if OPEN    
-        cvShowImage("타겟 인식", frame);
-        cvShowImage("처리된 영상", processed);
-
-        char c = cvWaitKey(30);
-        if (c == 'q') break;
+        cvShowImage("Raw Image", frame);
+        cvShowImage("Processed Image", processed);
 #endif    
         cvReleaseImage(&processed);
         cvClearMemStorage(storage);
         cvReleaseImage(&gray);
         cvReleaseImage(&blurred);
         cvReleaseImage(&edge);
-    }
-    
+    }    
     cvReleaseCapture(&capture);
 #if OPEN
-    cvDestroyWindow("타겟 인식");
-    cvDestroyWindow("처리된 영상");
+    cvDestroyWindow("Raw Image");
+    cvDestroyWindow("Processed Image");
 #endif
     cvReleaseMemStorage(&storage);
 #if CLIENT
-      // 스레드가 종료될 때까지 대기
+    // 스레드가 종료될 때까지 대기
     pthread_join(communicate_thread, NULL);
     close(fd);
 #endif
